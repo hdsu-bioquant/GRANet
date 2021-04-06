bb92c44ea280fe8c8787cdaddc466351cd52155e
andquintero <a.quintero@dkfz.de>
2021-04-06 15:37:10 +0200

    human atlas lung pre process atac sort fragments

--- scGRANet/human_atlas_lung/02_preprocess_scratacseq_data.R
+++ scGRANet/human_atlas_lung/02_preprocess_scratacseq_data.R
@@ -2,26 +2,18 @@
 #args <- commandArgs(TRUE)
 
 # p_scenic_adjacencies <- as.character(args[1])
-# p_archrproj          <- dirname(as.character(args[2]))
-# p_regulons           <- as.character(args[3])
-# genome               <- as.character(args[4])
-# importance_threshold <- as.numeric(args[5])/100
-# promoter_size        <- as.numeric(args[6])
-# min_regulon_size     <- as.numeric(args[7])
-# Ncores               <- as.numeric(args[8])
-# cellWithPeak         <- as.numeric(args[9])
+
 #------------------------------------------------------------------------------#
-# p_scenic_adjacencies <- "results/scrna/SCENIC/expr_mat.adjacencies.cor.tsv"
-# p_archrproj          <- "results/scatac/archr/Save-projcovid6/"
-# #p_scenic_corrmodules <- "results/integrated/TF_activity_method2/tfModules_asDF.RDS"
-# p_regulons           <- "results/integrated/TF_activity_method2/tfRegulons_asDF_byCell.RDS"
-genome               <- "hg38"
-# importance_threshold <- 50/100
-# promoter_size        <- 5000
-# min_regulon_size     <- 10
-# Ncores               <- 20 
-# cellWithPeak         <- 0.1 # Minimum fraction of cells for a given cell type, should have this peak to conserve it
-#rawFiles <- c("~/projects/mouse_brain_TF_activity/data/scatac/Lung2_62216_barcoded.bam")
+params <- list(
+  genome      = "hg38",
+  rawFilesdir = "/media/ag-cherrmann/projects/10_charite_covid19/subprojects/human_atlas_TF_activity/data/atac/fragments/",
+  rawFiles    = c("lung_SM-A62E9_rep1_fragments.bed.gz", "lung_SM-A8WNH_rep1_fragments.bed.gz", 
+                  "lung_SM-ACCPU_rep1_fragments.bed.gz", "lung_SM-JF1NZ_rep1_fragments.bed.gz"),
+  # rawFiles    = c("lung_SM-A62E9_rep1_fragments.txt", "lung_SM-A8WNH_rep1_fragments.txt", 
+  #                 "lung_SM-ACCPU_rep1_fragments.txt", "lung_SM-JF1NZ_rep1_fragments.txt"),
+  archerOutdir = "/media/ag-cherrmann/projects/10_charite_covid19/subprojects/human_atlas_TF_activity/data/atac/fragments_processed/"
+               )
+
 rawFiles <- c("/media/ag-cherrmann/projects/10_charite_covid19/subprojects/human_atlas_TF_activity/data/atac/fragments_processed/lung_SM-A62E9_rep1_fragments.txt",
               "/media/ag-cherrmann/projects/10_charite_covid19/subprojects/human_atlas_TF_activity/data/atac/fragments_processed/lung_SM-A8WNH_rep1_fragments.txt",
               "/media/ag-cherrmann/projects/10_charite_covid19/subprojects/human_atlas_TF_activity/data/atac/fragments_processed/lung_SM-ACCPU_rep1_fragments.txt",
@@ -31,20 +23,65 @@ rawFiles <- c("/media/ag-cherrmann/projects/10_charite_covid19/subprojects/human
 archerOutdir <- unique(dirname(rawFiles))
 #------------------------------------------------------------------------------#
 
-# Bam file did not have the barcode field by default, thus it was converted into a 
-# sam file, and the barcode was added using awk:
-# Rsamtools::asSam("~/projects/mouse_brain_TF_activity/data/scatac/Lung2_62216.bam")
-# awk '{ if($0 ~ "^@") {print $0} else {split($1,a,":"); gsub(/$/, "\tRG:Z:"a[1]); print} }' Lung2_62216.sam > Lung2_62216_barcoded.sam
-# and then converted back to a bam file:
-# Rsamtools::asBam("~/projects/mouse_brain_TF_activity/data/scatac/Lung2_62216_barcoded.sam")
+#------------------------------------------------------------------------------#
+#                               Index and sort fragments                       #
+#------------------------------------------------------------------------------#
 
-# Is is better to use samtools:
-# samtools view -h in.bam|awk '{ if($0 ~ "^@") {print $0} else {split($1,a,":"); gsub(/RG:Z:[^\t]*/, "RG:Z:"a[1]); print} }'|samtools view -b -o out.bam
+# Original files are not sorted, and bgzipped
+# first read them into a granges, sort, and then index
+
+makeFragTabix<-function(filepath,skip=0,rm.file=TRUE){
+  message("compressing the file with bgzip...")
+  zipped <- Rsamtools::bgzip(filepath,overwrite=TRUE)
+  
+  if(rm.file){file.remove(filepath)}
+  
+  message("making tabix index...")
+  Rsamtools::indexTabix(zipped,
+                        seq=1, start=2, end=3,
+                        skip=skip, comment="#", zeroBased=FALSE)
+  
+}
+
+rawFiles <- lapply(params$rawFiles, function(rawFile){
+  message("Start file: ", rawFile)
+  
+  from <-  file.path(params$rawFilesdir, rawFile)
+  baseto <- paste0(sub("\\..*", "", rawFile), ".txt.gz")
+  to <- file.path(params$archerOutdir, baseto)
+  
+  # Read data
+  x <- read.table(from)
+  x <- x[,1:5]
+  colnames(x) <- c("chr", "start", "end", "barcode", "count")
+  # Make granges and sort
+  gr <- makeGRangesFromDataFrame(x, keep.extra.columns = TRUE)
+  gr <-sort(gr, ignore.strand=TRUE)
+  # Save as table
+  message("save sorted granges as table: ", rawFile)
+  y <- as.data.frame(gr)
+  y <- y[,c(1,2,3,6,7)]
+  write_delim(y, file=to, delim = "\t", col_names = FALSE)
+  
+  # bgzip and index
+  message("bgzip and index: ", rawFile)
+  makeFragTabix(to)
+  
+  # final file
+  baseto <- paste0(sub("\\..*", "", rawFile), ".txt.bgz")
+  file.path(params$archerOutdir, baseto)
+  
+})
 
 
-# scanBamHeader(rawFiles)
-# myBam <- scanBam(rawFiles)
 
+
+
+
+
+#------------------------------------------------------------------------------#
+
+#------------------------------------------------------------------------------#
 
 library(ArchR)
 # library(patchwork)
