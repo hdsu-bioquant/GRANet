15748bd5fb082fe3b9dcf4878789afaeb1078c9e
AndresQ <a.quintero@dkfz.de>
2021-05-10 17:08:05 +0200

    class definition

--- R/coexpression_net.R
+++ R/coexpression_net.R
@@ -1,5 +1,34 @@
 
 
+
+#' Title
+#'
+#' @slot GeneExpression list.
+#' @slot ArchR list.
+#' @slot TFmotif_location list.
+#' @slot Coexprs_modules data.frame.
+#' @slot cssRegulons list.
+#' @slot SignFeatures data.frame.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+ButchR_NMF <- setClass(
+  Class = "GRANet",
+  slots = list(GeneExpression   = "list",
+               ArchR            = "list",
+               TFmotif_location = "list",
+               Coexprs_modules  = "data.frame",
+               cssRegulons      = "list",
+               SignFeatures = "data.frame" )
+
+)
+
+
+
+
+
 #' Title
 #'
 #' @param obj
