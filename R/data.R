2841ee6f11bbf673a6913352ea4549bbe6593f4c
AndresQ <a.quintero@dkfz.de>
2021-05-27 10:28:35 +0200

    TFs lists to data

---
+++ R/data.R
@@ -0,0 +1,80 @@
+#' Human transcription factors
+#'
+#' List curated by Van de Sande, B., Flerin, C., Davie, K. et al.
+#' A scalable SCENIC workflow for single-cell gene regulatory network analysis.
+#' Nat Protoc 15, 2247–2276 (2020). https://doi.org/10.1038/s41596-020-0336-2
+#'
+#'
+#' @format List of symbols of human transcription factors
+#' \describe{
+#'   \item{hs_hgnc_tfs}{1839 transcription factors symbols}
+#'   ...
+#' }
+#' @source \url{https://github.com/aertslab/pySCENIC/tree/master/resources}
+#'
+#' @usage data(hs_hgnc_tfs)
+#'
+"hs_hgnc_tfs"
+
+#' Curated human transcription factors
+#'
+#' List curated by Van de Sande, B., Flerin, C., Davie, K. et al.
+#' A scalable SCENIC workflow for single-cell gene regulatory network analysis.
+#' Nat Protoc 15, 2247–2276 (2020). https://doi.org/10.1038/s41596-020-0336-2
+#'
+#'
+#' @format Curated list of symbols of human transcription factors
+#' \describe{
+#'   \item{hs_hgnc_curated_tfs}{1390 transcription factors symbols}
+#'   ...
+#' }
+#' @source \url{https://github.com/aertslab/pySCENIC/tree/master/resources}
+#'
+#' @usage data(hs_hgnc_curated_tfs)
+#'
+"hs_hgnc_curated_tfs"
+
+
+#' Human transcription factors from Lambert 2018
+#'
+#' List curated by Van de Sande, B., Flerin, C., Davie, K. et al.
+#' A scalable SCENIC workflow for single-cell gene regulatory network analysis.
+#' Nat Protoc 15, 2247–2276 (2020). https://doi.org/10.1038/s41596-020-0336-2
+#'
+#' Originally published by Lambert SA, Jolma A, Campitelli LF, Das PK, Yin Y,
+#' Albu M, Chen X, Taipale J, Hughes TR, Weirauch MT. The Human Transcription
+#' Factors. Cell. 2018 Feb 8;172(4):650-665. doi: 10.1016/j.cell.2018.01.029.
+#' Erratum in: Cell. 2018 Oct 4;175(2):598-599. PMID: 29425488.
+#'
+#'
+#' @format List of symbols of human transcription factors from Lambert 2018
+#' \describe{
+#'   \item{lambert2018}{1639 transcription factors symbols}
+#'   ...
+#' }
+#' @source \url{https://github.com/aertslab/pySCENIC/tree/master/resources}
+#'
+#' @usage data(lambert2018)
+#'
+"lambert2018"
+
+
+#' Mouse transcription factors
+#'
+#' List curated by Van de Sande, B., Flerin, C., Davie, K. et al.
+#' A scalable SCENIC workflow for single-cell gene regulatory network analysis.
+#' Nat Protoc 15, 2247–2276 (2020). https://doi.org/10.1038/s41596-020-0336-2
+#'
+#'
+#' @format List of symbols of mouse transcription factors
+#' \describe{
+#'   \item{mm_mgi_tfs}{1721 transcription factors symbols}
+#'   ...
+#' }
+#' @source \url{https://github.com/aertslab/pySCENIC/tree/master/resources}
+#'
+#' @usage data(mm_mgi_tfs)
+#'
+"mm_mgi_tfs"
+
+
